75de9f35f2111bdcabe72fc0d68388ee459577e8
pythoncoderk <turntabroz@gmail.com>
2026-01-14 23:27:34 +0900

    atcoder

---
+++ 0114_10.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> vec1 = {1, 2, 3};
+    vector<int> vec2 = {1, 2, 3};
+
+    if (vec1 == vec2) cout << "OK" << endl;
+}
