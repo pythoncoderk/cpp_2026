028153c2d23fc703d83f3d8ddcf828a37b9cde5d
pythoncoderk <turntabroz@gmail.com>
2026-01-14 16:10:53 +0900

    atcoder

---
+++ 0114_4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> vec(3);
+
+    cin >> vec.at(0) >> vec.at(1) >> vec.at(2);
+
+    cout << vec.at(0) << vec.at(1) << vec.at(2) << endl;
+}
---
+++ 0114_9.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> vec = {1, 2, 3};
+
+    vec.pop_back();
+
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec.at(i) << endl;
+    }
+}
