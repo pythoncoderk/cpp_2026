cc98a24b4c3cfc02d9b57c8ef2614ee07f503688
pythoncoderk <turntabroz@gmail.com>
2026-01-22 06:37:42 +0900

    atcoder

---
+++ 0122_8.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n <= 60) cout << "OK" << endl;
+    else cout << "NG" << endl;
+}
---
+++ 0122_9.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h;
+    cin >> h;
+
+    cout << (h <= 60 ? "OK" : "NG") << endl;
+}
