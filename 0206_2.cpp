6cc0c577d523fa2034082b40c3c13cb85dc4e08f
pythoncoderk <turntabroz@gmail.com>
2026-02-06 06:57:55 +0900

    atcoder

---
+++ 0206_2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if (n >= m) cout << "Yes" << "\n";
+    else cout << "No" << "\n";
+    return 0;
+}
