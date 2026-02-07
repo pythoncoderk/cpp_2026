f1321191e43f71d1e9a9f139185efc42873f93ea
pythoncoderk <turntabroz@gmail.com>
2026-02-08 08:23:47 +0900

    atcoder

---
+++ 0208_1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, w;
+    cin >> a >> w;
+
+    if (a <= 15 && w >= 60) cout << "Yes" << "\n";
+    else cout << "No" << "\n";
+    return 0;
+}
---
+++ 0208_3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n >= 1000) cout << n - 100 << "\n";
+    else cout << n << "\n";
+    return 0;
+}
