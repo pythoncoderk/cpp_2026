4e223492c86fa624f13def1f9dc30b92e8795188
pythoncoderk <turntabroz@gmail.com>
2026-02-07 16:59:08 +0900

    atcoder

---
+++ 0207_1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m;
+    cin >> m;
+
+    if (m + 2 <= 12) cout << m + 2 << "\n";
+    else cout << m + 2 - 12 << "\n";
+    return 0;
+}
---
+++ 0207_2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m;
+    cin >> m;
+
+    cout << ((m - 1) + 2) % 12 + 1 << "\n";
+
+    return 0;
+}
