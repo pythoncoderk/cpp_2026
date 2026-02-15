c458a2b6345edaf1f4270409dc8b548a25f1ff4c
pythoncoderk <turntabroz@gmail.com>
2026-02-16 07:24:29 +0900

    atcoder

---
+++ 0215_3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << max(0, n - 120) << "\n";
+    return 0;
+}
---
+++ 0216_3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n >= 1000) cout << "Yes" << "\n";
+    else cout << "No" << "\n";
+    return 0;
+}
