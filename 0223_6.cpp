15e281f86656ca8fffda11af13d4e5983df3c6ff
pythoncoderk <turntabroz@gmail.com>
2026-02-23 21:13:29 +0900

    atcocder

--- 0223_6.cpp
+++ 0223_6.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    string s;
+    cin >> n >> s;
+    for (int i = 0; i < n; i++) {
+        cout << s[i] << "\n";
+    }
+    return 0;
 }
---
+++ 0223_7.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 30 && n < 35) cout << "M" << "\n";
+    else cout << n << "\n";
+    return 0;
+}
