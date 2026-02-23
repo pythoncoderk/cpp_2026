79ebe19e79d08c986718110a7928d1a7aa72ad8d
pythoncoderk <turntabroz@gmail.com>
2026-02-23 10:51:46 +0900

    atcoder

---
+++ 0223_3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double n;
+    cin >> n;
+
+    if (n <= 37.0) cout << "OK" << "\n";
+    else cout << "NG" << "\n";
+    return 0;
+}
---
+++ 0223_5.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (n + m == 21) cout << "Win" << "\n";
+    else cout << n + m << "\n";
+    return 0;
+
+}
