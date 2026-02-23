88182a463e40996f5e52621a70377082f58819db
pythoncoderk <turntabroz@gmail.com>
2026-02-24 06:51:43 +0900

    atcoder

---
+++ 0224_4.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    reverse(s.begin(), s.end());
+    cout << s << "\n";
+    return 0;
+}
