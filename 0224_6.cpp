09dfb05d13d03ed252edd8383fd5b89932fe86d8
pythoncoderk <turntabroz@gmail.com>
2026-02-24 22:09:49 +0900

    atcoder

---
+++ 0224_6.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s.size() >= 21) cout << "NG" << "\n";
+    else cout << "OK" << "\n";
+    return 0;
+}
---
+++ 0224_9.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s == "A") cout << 10 << "\n";
+    else if (s == "B") cout << 11 << "\n";
+    else cout << s << "\n";
+    return 0;
+}
