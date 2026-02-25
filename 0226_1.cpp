eb2af3250a4b71aaf9874f07a88ccd3a7e2ad154
pythoncoderk <turntabroz@gmail.com>
2026-02-26 07:18:58 +0900

    atcoder

---
+++ 0226_1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << (n >= 1000 ? n + 3 : n) << "\n";
+    return 0;
+}
---
+++ 0226_2.cpp
@@ -0,0 +1,11 @@
+#include <numbers>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 1000) cout << n + 3 << "\n";
+    else cout << n << "\n";
+    return 0;
+}
