8e793ba8ad754721d9a8036b052e653148e2ea25
pythoncoderk <turntabroz@gmail.com>
2026-03-04 07:11:45 +0900

    atcoder

---
+++ 0304_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m, v, f;
+    cin >> m >> v >> f;
+
+    cout << (m * (v * v)) / (2 * f) << "\n";
+    return 0;
+}
---
+++ 0304_2.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << ((a - 1) / b + 1) * c << "\n";
+    return 0;
+}
