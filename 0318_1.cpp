3e2bb01e808306f1d010f8f62cdfef176179efcd
pythoncoderk <turntabroz@gmail.com>
2026-03-18 07:04:35 +0900

    atcoder

---
+++ 0318_1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        cout << s;
+    }
+    cout << "\n";
+    return 0;
+}
---
+++ 0318_3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<string> v = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    };
+
+    int n;
+    cin >> n;
+    cout << v[n-1] << "\n";
+    return 0;
+}
