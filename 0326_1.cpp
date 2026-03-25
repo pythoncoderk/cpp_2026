ae6a2494f59b079e3a420ce648f392d3617cb161
pythoncoderk <turntabroz@gmail.com>
2026-03-26 07:29:27 +0900

    atcoder

---
+++ 0326_1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    for (int i = 0; i < a; i++) cout << "A";
+    for (int i = 0; i < b; i++) cout << "B";
+    for (int i = 0; i < c; i++) cout << "A";
+    cout << "\n";
+    return 0;
+}
---
+++ 0326_2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << string(a, 'A') + string(b, 'B') + string(c, 'A') << "\n";
+    return 0;
+}
