a9e46b64e18a6f05ee4a87128cf7b82e2fbfd095
pythoncoderk <turntabroz@gmail.com>
2026-03-30 07:37:43 +0900

    atcoder

---
+++ 0330_1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector<int> v = {a, b, c};
+
+    int mx = *max_element(v.begin(), v.end());
+    cout << mx << "\n";
+    return 0;
+}
---
+++ 0330_2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+
+    cout << "Gold" << " " << s1 << "\n";
+    cout << "Silver" << " " << s2 << "\n";
+    cout << "Bronze" << " " << s3 << "\n";
+    return 0;
+}
