d844167f6ab7d01e3c11dbbc7f8bbfca3592c62b
pythoncoderk <turntabroz@gmail.com>
2026-03-30 23:09:14 +0900

    atcoder

--- backup/0301_1.cpp
+++ backup/0301_1.cpp
@@ -1,3 +1,11 @@
-//
-// Created by turnt on 2026/03/01.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if (n >= m) cout << "Thank you" << "\n";
+    else cout << m - n << "\n";
+    return 0;
+}
