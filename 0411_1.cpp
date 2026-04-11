1c850f6cd1a85458b2b5b7daf17abb12dcfc93e1
pythoncoderk <turntabroz@gmail.com>
2026-04-11 17:24:27 +0900

    atcoder

---
+++ 0411_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a * 2 + b * 2 << "\n";
+    return 0;
+}
---
+++ 0411_3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h;
+    cin >> h;
+
+    cout << (h < 12 ? "AM" : "PM") << "\n";
+    return 0;
+}
