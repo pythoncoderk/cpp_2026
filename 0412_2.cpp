f903c0398ab954166eec73406dc2b937d8870a03
pythoncoderk <turntabroz@gmail.com>
2026-04-12 07:07:29 +0900

    atcoder

---
+++ 0412_2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    cin >> y >> m >> d;
+
+    cout << y << "/" << m << "/" << d << "\n";
+    return 0;
+}
---
+++ 0412_3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    cin >> y >> m >> d;
+
+    printf("%d/%d/%d", y, m, d);
+    cout << "\n";
+    return 0;
+}
