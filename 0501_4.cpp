5a11226f0b5b76cffa2c88126270a0a831e61683
pythoncoderk <turntabroz@gmail.com>
2026-05-01 08:21:47 +0900

    atcoder

---
+++ 0501_4.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, a, b, c;
+    cin >> n >> a >> b >> c;
+    cout << (n >= a + b + c ? "OK" : "NG") << "\n";
+    return 0;
+}
---
+++ 0501_6.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << (n / 60) * m << "\n";
+    return 0;
+
+}
