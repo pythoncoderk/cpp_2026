040ecea3f69d87226041feba33f55a74b48ed811
pythoncoderk <turntabroz@gmail.com>
2026-05-04 08:06:00 +0900

    atcoder

---
+++ 0503_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << (m + 30 - 1) / 30 * n << "\n";
+    return 0;
+}
---
+++ 0504_1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 11) cout << 1 << "\n";
+    else if (n == 12) cout << 2 << "\n";
+    else cout << n + 2 << "\n";
+    return 0;
+}
