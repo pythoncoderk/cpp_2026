654ffef989d71312b2921900e0be306d2d09febe
pythoncoderk <turntabroz@gmail.com>
2026-05-06 16:54:23 +0900

    atcoder

---
+++ 0506_3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m, p;
+    cin >> n >> m >> p;
+
+    cout << n - m * p << "\n";
+    return 0;
+}
---
+++ 0506_5.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, w;
+    cin >> a >> w;
+
+    cout << (a <= 15 && w >= 60 ? "Yes" : "No") << "\n";
+    return 0;
+}
