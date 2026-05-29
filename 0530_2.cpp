632197a36466116daaff74039e51ad915902eae4
pythoncoderk <turntabroz@gmail.com>
2026-05-30 08:54:30 +0900

    atcoder

---
+++ 0530_2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if (a < c) {
+        cout << "Yes" << "\n";
+    } else {
+        if (a == c) {
+            if (d <= b) {
+                cout << "Yes" << "\n";
+            } else {
+                cout << "No" << "\n";
+            }
+        } else {
+            cout << "Yes" << "\n";
+        }
+    }
+}
---
+++ 0530_3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if (a > c) {
+        cout << "Yes" << "\n";
+    } else if (a == c && b >= d) {
+        cout << "Yes" << "\n";
+    } else {
+        cout << "No" << "\n";
+    }
+}
