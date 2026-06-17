22e87d3130d8a75cc795f81c14ec81f6e2e2cca1
pythoncoderk <turntabroz@gmail.com>
2026-06-18 07:36:08 +0900

    atcoeder

---
+++ 0618_1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < 3; i++) {
+        cout << s[i];
+    }
+    cout << "\n";
+    return 0;
+}
