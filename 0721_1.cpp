1364454a40167d1e1b41f6792242e30fef30ea20
pythoncoderk <turntabroz@gmail.com>
2026-07-22 07:19:04 +0900

    atcoder

---
+++ 0721_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n , m;
+    cin >> m >> n;
+
+    cout << n % m << "\n";
+    return 0;
+}
---
+++ 0722_1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    reverse(s.begin(), s.end());
+    cout << s << "\n";
+    return 0;
+}
