73d69bf5369ed751e1fc1eb55db230075559cb8c
pythoncoderk <turntabroz@gmail.com>
2026-08-24 08:06:24 +0900

    atcoder

--- 0824_1.cpp
+++ 0824_1.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
+    
+    if (n - m >= 0) {
+        cout << n - m << endl;
+    } else {
+        cout << 1 << endl;
+    }
 
-    cout << n * m << "\n";
     return 0;
 }
